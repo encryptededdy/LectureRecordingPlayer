608615fcc8f5c5119fc9db36cbb3b3f4b870bf01
Edward Zhang <edward@zhang.nz>
2017-11-09 21:33:49 +1300

    Added Open Source Licenses view

--- app/src/main/java/nz/zhang/lecturerecordingplayer/MainActivity.kt
+++ app/src/main/java/nz/zhang/lecturerecordingplayer/MainActivity.kt
@@ -8,6 +8,7 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.afollestad.materialdialogs.MaterialDialog
 import com.tonyodev.fetch.Fetch
 import nz.zhang.lecturerecordingplayer.recordings.RecordingStore
 
@@ -45,6 +46,30 @@ class MainActivity : AppCompatActivity() {
     fun showCanvasScraper(view: View) {
         val scraperIntent = Intent(this, CanvasScraperActivity::class.java)
         startActivity(scraperIntent)
+    }
+
+    fun showOSSLicenses(view: View) {
+        MaterialDialog.Builder(this)
+                .title(R.string.open_source_licenses)
+                .content("Libraries used in this software are licensed under various licenses;\n\nRetrofit2: Copyright 2013 Square, Inc., Apache License 2.0\nFetch: Copyright 2017 Tonyo Francis., Apache License 2.0\nMaterial-Dialogs: MIT License\nOKHttp3: Apache License 2.0\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                        "you may not use this file except in compliance with the License.\n" +
+                        "You may obtain a copy of the License at\n" +
+                        "\n" +
+                        "   http://www.apache.org/licenses/LICENSE-2.0\n" +
+                        "\n" +
+                        "Unless required by applicable law or agreed to in writing, software\n" +
+                        "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                        "See the License for the specific language governing permissions and\n" +
+                        "limitations under the License.\n\n" +
+                        "The MIT License\n" +
+                        "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n" +
+                        "\n" +
+                        "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n" +
+                        "\n" +
+                        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n")
+                .positiveText(getString(R.string.ok))
+                .show()
     }
 
 }
