df7c6b336e14a1d4d4a1f8afa71ed9155f854d1b
Edward Zhang <edward@zhang.nz>
2018-06-01 13:44:48 +1200

    Updated version number, release to Play (1.4)

--- app/src/main/java/nz/zhang/lecturerecordingplayer/MainActivity.kt
+++ app/src/main/java/nz/zhang/lecturerecordingplayer/MainActivity.kt
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 const val PREFS_NAME = "RecordingStorage"
-const val VERSION = "Beta 1.3"
+const val VERSION = "Beta 1.4"
 
 class MainActivity : AppCompatActivity() {
 
